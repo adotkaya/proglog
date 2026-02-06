bf28b7db830971b9ad64344444c6b668322dd28a
Alkim Kaya <adotkaya@gmail.com>
2026-02-06 16:28:26 +0300

    grcpServer init

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,11 +2,37 @@ package main
 
 import (
 	"log"
+	"net"
+	"os"
 
+	ilog "github.com/adotkaya/proglog/internal/log"
 	"github.com/adotkaya/proglog/internal/server"
 )
 
 func main() {
-	srv := server.NewHTTPServer(":8080")
-	log.Fatal(srv.ListenAndServe())
+	l, err := net.Listen("tcp", ":8400")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	dir, err := os.MkdirTemp("", "proglog")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	clog, err := ilog.NewLog(dir, ilog.Config{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	cfg := &server.Config{
+		CommitLog: clog,
+	}
+
+	srv, err := server.NewGRPCServer(cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Server listening on :8400")
+	log.Fatal(srv.Serve(l))
 }
